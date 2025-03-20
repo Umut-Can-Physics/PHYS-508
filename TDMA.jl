a64eb6eaea2e9cb027e1950d77ad345dad611f3d
Umut-Can-Physics <umutcanturhan3@gmail.com>
2025-03-20 15:26:14 +0300

    Update 2

--- TDMA.jl
+++ TDMA.jl
@@ -36,13 +36,17 @@ function Get_ABCD_Values(c, d, N, ϵ, method)
 
     # Initialization of arrays 
     A=zeros(N);B=zeros(N);C=zeros(N);F=zeros(N);
-    γ=zeros(N);R = (a.*h)./ϵ
+    γ=zeros(N);R=(a.*h)./ϵ
     if method==1
         γ = ones(N)
     elseif method==2
         γ = (0.5*abs.(R)).+1
     elseif method==3
+        inf_point = findall(n->n==0,R)
         γ = 0.5*R.*coth.(0.5*R)
+        if inf_point != length(0)
+            γ[only(inf_point)] = 1
+        end
     end
 
     for k in 2:N-1
@@ -112,6 +116,7 @@ end
 # Thissection includes running of the functions and plot the results
 #-#
 
+# use odd numbers for N
 N=15;ϵ=0.1
 c=-1;d=1
 method = 1
@@ -119,66 +124,3 @@ A, B, C, F, x, x_range = Get_ABCD_Values(c, d, N, ϵ, method)
 U = TDMA(A,B,C,F)
 plot(x_range, U)
 plot!(x_range,exact(x, ϵ),linestyle=:dash)
-
-# Start and end points of grid line
-c=-1;d=1
-
-# Effect of the grid refinement
-N_list=[10,20,40,80]; ϵ=0.01
-P1 = plot()
-p1_title = latexstring("\$ ϵ=$(ϵ) \$")
-error_list_1 = []
-for N in N_list
-    A, B, C, F, x, x_range = Get_ABCD_Values(c, d, N, ϵ)
-    # Run TDMA method
-    U = TDMA(A,B,C,F)
-    # Get exact solution
-    U_exact = exact(x,ϵ)
-    # Error
-    errors = push!(error_list_1, abs.(U_exact.-U))
-    # ! means hold on the plot to show two of plots simultaneously for comparing exact and discrete solutions
-    plot!(P1, x_range,U,title=p1_title,label=latexstring("\$ N=$(N) \$"),xlabel=L"x",dpi=500)
-end
-savefig(P1, "Effect of grid refinement.png")
-
-# Plot errors for grid refinement
-errors_grid_refinement = plot(error_list_1[1], ylabel=L"||e||_\infty", label=latexstring("\$ N=$(N_list[1])\$"),title=L"||e||_\infty=max_{1 \leq i \leq N}|u(x_i)-u_i|", dpi=500)
-plot!(error_list_1[2], ylabel=L"||e||_\infty", label=latexstring("\$ N=$(N_list[2])\$"), dpi=500)
-plot!(error_list_1[3], ylabel=L"||e||_\infty", label=latexstring("\$ N=$(N_list[3])\$"), dpi=500)
-plot!(error_list_1[4], ylabel=L"||e||_\infty", label=latexstring("\$ N=$(N_list[4])\$"), dpi=500)
-savefig(errors_grid_refinement,"Errors for Grid Refinement.png")
-
-# Effect of the ϵ refinement
-c=-1;d=1
-ϵ_list=[1, 1e-1, 1e-2, 1e-3]; N=40
-P2 = plot()
-p2_title = latexstring("\$ N=$(N) \$")
-error_list_2=[]
-for ϵ in ϵ_list
-    A, B, C, F, x, x_range = Get_ABCD_Values(c, d, N, ϵ)
-    # Run TDMA method
-    U = TDMA(A,B,C,F)
-    # Get exact solution
-    U_exact = exact(x,ϵ)
-    # Error
-    errors = push!(error_list_2, abs.(U_exact.-U))
-    # ! means hold on the plot to show two of plots simultaneously for comparing exact and discrete solutions
-    plot!(P2, x_range,U,title=p2_title,label=latexstring("\$ ϵ=$(ϵ) \$"),xlabel=L"x",dpi=500)
-end
-savefig(P2, "Effect of epsilon refinement.png")
-
-# Plot errors for ϵ refinement
-errors_epsilon_refinement = plot(error_list_2[1], ylabel=L"||e||_\infty", label=latexstring("\$ ϵ=$(ϵ_list[1])\$"),title=L"||e||_\infty=max_{1 \leq i \leq N}|u(x_i)-u_i|",dpi=500)
-plot!(error_list_2[2], ylabel=L"||e||_\infty", label=latexstring("\$ ϵ=$(ϵ_list[2])\$"),dpi=500)
-plot!(error_list_2[3], ylabel=L"||e||_\infty", label=latexstring("\$ ϵ=$(ϵ_list[3])\$"),dpi=500)
-plot!(error_list_2[4], ylabel=L"||e||_\infty", label=latexstring("\$ ϵ=$(ϵ_list[4])\$"),dpi=500)
-savefig(errors_epsilon_refinement,"Errors for Epsilon Refinement.png")
-
-# Get exact solution for a sample parameter
-N=41;ϵ=1e-3
-_, _, _, _, x, x_range = Get_ABCD_Values(c, d, N, ϵ)
-U_exact = exact(x,ϵ)
-p_title = latexstring("\$ N=$(N), ϵ=$(ϵ) \$")
-# Plot TDMA result and exact solution of the differential equation as a function of x range with the proper plot title
-exact_result = plot(x_range, U_exact, linestyle=:dash, label="Exact", linecolor=:black,dpi=500,title=p_title,xlabel=L"x")
-savefig(exact_result,"Exact Result.png")
